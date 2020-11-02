e9eab3366d962ff0e4eafaba84f4830b3398ba3b
Thomas Lotze <thomas@thomas-lotze.net>
2020-11-02 12:44:46 +0100

    add ILanguageSelectable to mark content mirrored by translated mirrors
    This is meant for content that isn't translatable in itself but should
    nevertheless display a language selector in order to (eventually) be
    redirected to its copy in the target language's mirror.

--- src/collective/mirror/interfaces.py
+++ src/collective/mirror/interfaces.py
@@ -1,7 +1,17 @@
 """Module where all interfaces, events and exceptions live."""
 
+from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class ICollectiveMirrorLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class ILanguageSelectable(Interface):
+    """Marker interface for non-translatables that should allow language selection.
+
+    This makes sense in the case of mirrors that are translations of each other but
+    refer to a master folder that is not itself translated.
+
+    """
